9a60fbcae16eef011c8d4617d1ea348ea93b3eb5
marchrys <mar.chrysikopoulos@gmail.com>
2021-04-11 18:26:21 +0200

    added move to the player and added a treasure

--- js/game.js
+++ js/game.js
@@ -1,6 +1,12 @@
 // create a new scene named "Game"
 let gameScene = new Phaser.Scene('Game');
 	
+// some parameters for our scene (our own customer variables - these are NOT part of the Phaser API)
+gameScene.init = function() {
+  this.playerSpeed = 1.5;
+  this.enemyMaxY = 280;
+  this.enemyMinY = 80;
+}
 // load asset files for our game
 gameScene.preload = function() {
  
@@ -14,15 +20,27 @@ gameScene.preload = function() {
 // executed once, after assets were loaded
 gameScene.create = function() {
  
-   // background
-   this.add.sprite(320, 180, 'background');
-
-   // player
+  // background
+  this.add.sprite(320, 180, 'background');
+  // player
   this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
- 
   // scale down
   this.player.setScale(0.5);
+
+  // goal
+  this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
+  this.treasure.setScale(0.6);
 }
+
+// executed on every frame (60 times per second)
+gameScene.update = function() {
+ 
+  // check for active input
+  if (this.input.activePointer.isDown) {
+    // player walks
+    this.player.x += this.playerSpeed;
+  }
+};
  
 // our game's configuration
 let config = {
