85804ec5fedcb6a0d16d0d4b80260efe0799dd4c
marchrys <mar.chrysikopoulos@gmail.com>
2021-04-11 18:17:12 +0200

    preloaded assets and added a player

--- js/game.js
+++ js/game.js
@@ -1,5 +1,28 @@
 // create a new scene named "Game"
 let gameScene = new Phaser.Scene('Game');
+	
+// load asset files for our game
+gameScene.preload = function() {
+ 
+  // load images
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+  this.load.image('dragon', 'assets/dragon.png');
+  this.load.image('treasure', 'assets/treasure.png');
+};
+ 
+// executed once, after assets were loaded
+gameScene.create = function() {
+ 
+   // background
+   this.add.sprite(320, 180, 'background');
+
+   // player
+  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
+ 
+  // scale down
+  this.player.setScale(0.5);
+}
  
 // our game's configuration
 let config = {
