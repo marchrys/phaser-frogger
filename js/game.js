8630a6233131ce355202fa7a3752f6cc77b5b8c5
Marios CHRYSIKOPOULOS <Marios CHRYSIKOPOULOS@cdg67.local>
2021-04-13 16:15:48 +0200

    completed tutorial

--- js/game.js
+++ js/game.js
@@ -45,11 +45,27 @@ gameScene.create = function() {
 
   // scale enemies
   Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
+
+  // set speeds
+  Phaser.Actions.Call(this.enemies.getChildren(), function(enemy) {
+    enemy.speed = Math.random() * 1 + 1;
+  }, this);
+
+  // player is alive
+  this.isPlayerAlive = true;
+    
+  // reset camera effects
+  this.cameras.main.resetFX();
 }
 
 // executed on every frame (60 times per second)
 gameScene.update = function() {
  
+  // only if the player is alive
+  if (!this.isPlayerAlive) {
+    return;
+  }
+
   // check for active input
   if (this.input.activePointer.isDown) {
     // player walks
@@ -60,14 +76,52 @@ gameScene.update = function() {
   if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
     this.gameOver();
   }
+
+  // enemy movement
+  let enemies = this.enemies.getChildren();
+  let numEnemies = enemies.length;
+ 
+  for (let i = 0; i < numEnemies; i++) {
+ 
+    // move enemies
+    enemies[i].y += enemies[i].speed;
+ 
+    // reverse movement if reached the edges
+    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
+      enemies[i].speed *= -1;
+    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
+      enemies[i].speed *= -1;
+    }
+
+    // enemy collision
+    if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
+      this.gameOver();
+      break;
+    }
+  }
 };
 
 // end the game
 gameScene.gameOver = function() {
  
-  // restart the scene
-  this.scene.start();
-}
+  // flag to set player is dead
+  this.isPlayerAlive = false;
+ 
+  // shake the camera
+  this.cameras.main.shake(500);
+ 
+  // fade camera
+  this.time.delayedCall(250, function() {
+    this.cameras.main.fade(250);
+  }, [], this);
+ 
+  // restart game
+  this.time.delayedCall(500, function() {
+    this.scene.start();
+  }, [], this);
+ 
+  
+};
  
 // our game's configuration
 let config = {
